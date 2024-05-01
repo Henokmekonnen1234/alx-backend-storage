64cb11be6acaa2ece580723d395cde1fdd428ad7
Henokmekonnen1234 <henokmac2425@gmail.com>
2024-05-01 22:02:26 +0300

    redit db with python

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,27 +1,32 @@
-import requests
+#!/usr/bin/env python3
+""" Redis Module """
+
+from functools import wraps
 import redis
-import time
+import requests
+from typing import Callable
 
-# Connect to Redis
-r = redis.Redis(host='localhost', port=6379, db=0)
+redis_ = redis.Redis()
 
+
+def count_requests(method: Callable) -> Callable:
+    """ Decortator for counting """
+    @wraps(method)
+    def wrapper(url):  # sourcery skip: use-named-expression
+        """ Wrapper for decorator """
+        redis_.incr(f"count:{url}")
+        cached_html = redis_.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
+        html = method(url)
+        redis_.setex(f"cached:{url}", 10, html)
+        return html
+
+    return wrapper
+
+
+@count_requests
 def get_page(url: str) -> str:
-    # Check if the URL is in the cache
-    cached_content = r.get(url)
-    if cached_content:
-        print("Retrieving from cache...")
-        return cached_content.decode('utf-8')
-
-    # If not in cache, fetch the content
-    print("Fetching from URL...")
-    response = requests.get(url)
-    content = response.text
-
-    # Store the content in cache with expiration time
-    r.setex(url, 10, content)
-
-    # Update the access count for this URL
-    count_key = f"count:{url}"
-    r.incr(count_key)
-
-    return content
+    """ Obtain the HTML content of a  URL """
+    req = requests.get(url)
+    return req.text
